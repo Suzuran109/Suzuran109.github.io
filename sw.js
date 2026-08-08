4edbc0e6dea4d0661fcdc1d47f04b81adb085b9a
Suzuran109 <caojianhong1101@163.com>
2026-08-08 11:13:23 +0800

    Site updated: 2026-08-08 11:13:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1786157823287";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1786158796952";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
